e3b23d3a3a756a891e949979d0377d5a7eed97ea
chandan <chandan.punyakumar@pearson.com>
2018-12-07 13:29:36 -0700

    completed day7 part1

---
+++ src/main/java/com/cckumarr/day7/Day7Part1.java
@@ -0,0 +1,88 @@
+package main.java.com.cckumarr.day7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+//--- Day 7: The Sum of Its Parts ---
+public class Day7Part1 {
+  public static void main(String args[]) throws FileNotFoundException {
+    ArrayList<Node> readyToBeProcessed = new ArrayList<>();
+    ArrayList<Node> doneList = new ArrayList<>();
+    ArrayList<Node> allNodes = new ArrayList<>();
+    HashMap<String,Node> map = new HashMap<>();
+
+    //scan and add all the elements to the map
+    Scanner input = new Scanner(new File("resources/day7input.txt"));
+    while(input.hasNext()){
+      String str = input.nextLine();
+      String[] split = str.split(" ");
+
+      String prev = split[1];
+      String next = split[7];
+      Node prevNode=null,nextNode=null;
+
+      if(map.get(prev) == null){
+        prevNode = new Node(prev);
+        map.put(prev,prevNode);
+      }else{
+        prevNode = map.get(prev);
+      }
+
+      if(map.get(next) == null){
+        nextNode = new Node(next);
+        map.put(next,nextNode);
+      }else{
+        nextNode = map.get(next);
+      }
+
+      prevNode.setConnectsTo(nextNode);
+      nextNode.setRequirements(prevNode);
+
+    }
+
+    //find the nodes with no requirements
+    for(Node n : map.values()){
+      if(n.getRequirements().isEmpty()){
+        if(!readyToBeProcessed.contains(n)) {
+          readyToBeProcessed.add(n);
+        }
+      }
+    }
+    Collections.sort(readyToBeProcessed);
+
+    /*add c to done
+    remove c from readytobeprocessed
+    loop through c's connectsto
+      check to if the requirements of that node is fulfilled
+      add element to readytobeprocessed
+    sort the readytobeprocessed*/
+    while(!readyToBeProcessed.isEmpty()){
+      Node node = readyToBeProcessed.remove(0);
+      System.out.print(node.getName());
+      doneList.add(node);
+      Boolean contains;
+      for(Node n : node.getConnectsTo()){
+        contains=true;
+        for(Node requirementsCheck : n.getRequirements()){
+          if(!doneList.contains(requirementsCheck)){
+            contains=false;
+            break;
+          }
+        }
+        if(contains==true){
+          if(!readyToBeProcessed.contains(n)) {
+            readyToBeProcessed.add(n);
+          }
+        }
+      }
+      Collections.sort(readyToBeProcessed);
+    }
+  }
+}
+
+
+//Answer : IOFSJQDUWAPXELNVYZMHTBCRGK
