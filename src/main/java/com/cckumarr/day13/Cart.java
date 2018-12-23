b39234067b334b4723ac70057726da976d739ab2
chandan <chandan.punyakumar@pearson.com>
2018-12-23 14:42:34 -0700

    completed day13part1

---
+++ src/main/java/com/cckumarr/day13/Cart.java
@@ -0,0 +1,8 @@
+package main.java.com.cckumarr.day13;
+
+public class Cart {
+  public char previousValue;
+  public String directionAtIntersection;
+  public int locationX;
+  public int locationy;
+}
